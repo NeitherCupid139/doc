c64c2866b00451622749d60171810d98e1ed2862
Creed Chung <75154332+NeitherCupid139@users.noreply.github.com>
2024-06-12 23:38:38 +0800

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -37,7 +37,7 @@ const config: Config = {
           sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/NeitherCupid139/notes/tree/main/docs",
+          editUrl: "https://github.com/NeitherCupid139/notes/tree/main",
         },
         theme: {
           customCss: "./src/css/custom.css",
